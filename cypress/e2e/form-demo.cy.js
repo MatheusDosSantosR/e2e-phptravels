f23b8f110e166a96a6651e49a3386fb750baf5b1
Matheus <matheus.santos@appfacilita.com>
2022-10-22 15:35:18 -0300

    add test - check place holder

--- cypress/e2e/form-demo.cy.js
+++ cypress/e2e/form-demo.cy.js
@@ -1,11 +1,74 @@
-describe('empty spec', () => {
-    beforeEach(() => {
+beforeEach(() => {
+    cy.visit('https://phptravels.com/demo/')
+})
+describe('Check place holders', () => {
+    it('Resolution HD 1368 - 768', () => {
         cy.viewport(1368, 768)
+        cy.get('.first_name')
+            .should('have.attr', 'placeholder', 'First Name')
+            .should('have.attr', 'class', 'first_name input mb1')
+        cy.get('.last_name')
+            .should('have.attr', 'placeholder', 'Last Name')
+            .should('have.attr', 'class', 'last_name input mb1')
+        cy.get('.business_name')
+            .should('have.attr', 'placeholder', 'Business Name')
+            .should('have.attr', 'class', 'business_name input mb1')
+        cy.get('.email')
+            .should('have.attr', 'placeholder', 'Email')
+            .should('have.attr', 'placeholder', 'Email')
+        cy.get('#demo')
+            .should('have.attr', 'class', 'btn w100')
+            .should('have.text', 'Submit')
+        cy.get('#number')
+            .should('have.attr', 'type', 'text')
+            .should('have.attr', 'placeholder', 'Result ?')
     })
 
-    it('Form filling.', () => {
-        cy.visit('https://phptravels.com/demo/')
-        cy.scrollTo('top')
+    it('Resolution Full HD 1920- 1080.', () => {
+        cy.viewport(1920, 1080)
+        cy.get('.first_name')
+            .should('have.attr', 'placeholder', 'First Name')
+            .should('have.attr', 'class', 'first_name input mb1')
+        cy.get('.last_name')
+            .should('have.attr', 'placeholder', 'Last Name')
+            .should('have.attr', 'class', 'last_name input mb1')
+        cy.get('.business_name')
+            .should('have.attr', 'placeholder', 'Business Name')
+            .should('have.attr', 'class', 'business_name input mb1')
+        cy.get('.email')
+            .should('have.attr', 'placeholder', 'Email')
+            .should('have.attr', 'placeholder', 'Email')
+        cy.get('#demo')
+            .should('have.attr', 'class', 'btn w100')
+            .should('have.text', 'Submit')
+        cy.get('#number')
+            .should('have.attr', 'type', 'text')
+            .should('have.attr', 'placeholder', 'Result ?')
+    })
+})
+
+describe('Form filling', () => {
+    it('Resolution HD 1368 768', () => {
+        cy.viewport(1368, 768)
+        cy.get('.first_name').type('name', { force: true })
+        cy.get('.last_name').type('last name', { force: true })
+        cy.get('.business_name').type('bussines', { force: true })
+        cy.get('.email').type('teste@teste.com', { force: true })
+            //Somar numeros
+        cy.get('#numb1').invoke('text').then((number1) => {
+            cy.get('#numb2').invoke('text').then((number2) => {
+                console.log(typeof number1, typeof number2)
+                let result = parseInt(number1) + parseInt(number2)
+                cy.get('#number').type(result, { force: true })
+            })
+        })
+        cy.get('#demo').click({ force: true })
+        cy.get('.animation-ctn').should('be.visible')
+        cy.get('.completed > h2.text-center > strong').should('have.text', ' Thank you!')
+    })
+
+    it('Resolution Full HD  - 1920 - 1080', () => {
+        cy.viewport(1920, 1080)
         cy.get('.first_name').type('name', { force: true })
         cy.get('.last_name').type('last name', { force: true })
         cy.get('.business_name').type('bussines', { force: true })
